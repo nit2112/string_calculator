ce8a24c2dffb7ebddd9c8cba5d14e2f62305c10e
bishtnitish <nbisht@connamara.com>
2024-05-31 23:34:25 +0530

    Method for adding two string and empty string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,3 +1,10 @@
 class StringCalculator
-
+  def self.add(input)
+    if input.empty?
+      0
+    else
+      numbers = input.split(",").map {|x| x.to_i}
+      numbers.reduce(:+)
+    end
+  end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -2,5 +2,16 @@ require "string_calculator"
 
 describe StringCalculator do
 
+  context "when input contains two numbers" do
+    it "returns their sum" do
+      expect(StringCalculator.add("1,2")).to eq(3)
+    end
+  end
+
+  context "given an empty string" do
+    it "returns zero" do
+      expect(StringCalculator.add("")).to eq(0)
+    end
+  end
 
 end
